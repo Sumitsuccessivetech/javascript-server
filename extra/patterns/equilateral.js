3c7d603dc9514c6ac178ccb8276b4626ed87aab2
Sumit Upadhyay <sumit.upadhyay@tr03.successivetech.com>
2020-10-15 18:04:56 +0530

    Docs: Add files

---
+++ extra/patterns/equilateral.js
@@ -0,0 +1,22 @@
+let a,b;
+equilateral(5)
+function equilateral(a)
+{
+console.log("print a equilateral triangle "+a);
+
+if(a<2 && a>10)
+return(0);
+
+for(let i=1; i<=a; i++)
+{
+b=" ";
+for(let j=i; j<=a; j++)
+{
+b+=" ";
+}
+for (let k=i; k>0; k--){
+b+="* ";
+}
+console.log(b);
+}
+}
