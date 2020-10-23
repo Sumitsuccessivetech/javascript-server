3637082f918b7b1a7337e1749f18f5a0e922f0b8
Sumit Upadhyay <sumit.upadhyay@tr03.successivetech.com>
2020-10-23 14:09:18 +0530

    feet: Create Email Validation and Permission

--- extra/utills/permission.js
+++ extra/utills/permission.js
@@ -1,28 +1,20 @@
 
- const permissions =
+const permissions =
 {
-'getUsers': {
-all: ['head-trainer'],
-read : ['trainee', 'trainer'],
-write : ['trainer'],
-Delete: [],
-}
-}
-
-function hasPermission(moduleName,role,permissionType)
-
-{
-    let inc=(permissions[moduleName][permissionType].includes(role))
-    if(inc===true){
-        return true;
-    }else{
-        return false;
+    'getUsers': {
+        all: ['head-trainer'],
+        read: ['trainee', 'trainer'],
+        write: ['trainer'],
+        Delete: [],
     }
-   
 }
 
-let ret=hasPermission('getUsers',"trainer","read");
-console.log(ret);
-ret=hasPermission('getUsers',"trainee","write");
-console.log(ret);
+function hasPermission(moduleName, role, permissionType) {
+    return (permissions[moduleName][permissionType].includes(role))
+}
+
+const result1 = hasPermission('getUsers', "trainer", "read");
+console.log(result1);
+const result2 = hasPermission('getUsers', "trainee", "write");
+console.log(result2);
 
--- extra/utills/validation.js
+++ extra/utills/validation.js
@@ -1,34 +1,34 @@
 const users = [
     {
-    traineeEmail: 'sumit.upadhyay@successive.tech',
-    reviewerEmail: 'chirag.arora@successive.tech',
+        traineeEmail: 'sumit.upadhyay@successive.tech',
+        reviewerEmail: 'chirag.arora@successive.tech',
     },
     {
-    traineeEmail: '@sumitsuccessive.tech',
-    reviewerEmail: '@chiragsuccesive.tech',
+        traineeEmail: '@sumitsuccessive.tech',
+        reviewerEmail: '@chiragsuccesive.tech',
     },
-    ];
-    
-    function validateEmail(email){
+];
+
+function validateEmail(email) {
     const pattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
- 
-    if(pattern.test(email)){}
-    }
-    function validateUsers(userData){
+
+    return (pattern.test(email))
+
+
+}
+function validateUsers(userData) {
     let validUser = [];
     let invalidUser = [];
     userData.forEach(userData => {
-        const {traineeEmail, reviewerEmail} =userData;
-    if(validateEmail(traineeEmail) && validateEmail(reviewerEmail) )
-    {
-        validUser.push(userData);
-    }else
-    {
-        invalidUser.push(userData);
-    }
+        const { traineeEmail, reviewerEmail } = userData;
+        if (validateEmail(traineeEmail) && validateEmail(reviewerEmail)) {
+            validUser.push(userData);
+        } else {
+            invalidUser.push(userData);
+        }
     });
-    console.log([validUser.length]  + " are valid users:", validUser);
-    console.log([invalidUser.length] +  " are  invalid users:", invalidUser);
-    }
-    validateUsers(users);
+    console.log([validUser.length] + " are valid users:", validUser);
+    console.log([invalidUser.length] + " are  invalid users:", invalidUser);
+}
+validateUsers(users);
 
