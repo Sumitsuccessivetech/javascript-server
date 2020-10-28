b8fa4743c351208aff1d85d55e821653b2fddd7e
unknown <sumit kumar upadhyay>
2020-10-28 17:59:44 +0530

    feat: HTTP Status code and handlers

---
+++ src/libs/routes/index.ts
@@ -0,0 +1,2 @@
+export {default as notFoundHandler } from './notFoundRoutes';
+export {default as errorHandler } from './errorHandler';
--- src/server.ts
+++ src/server.ts
@@ -1,35 +1,7 @@
-/*import * as express from "express";
-class server{
-    app
-    constructor(private config){
-        this.app=express()
-    }
-    bootstrap(){
-        this.setupRoutes()
-        return this;
-    }
-
-    setupRoutes(){
-        const {app}= this;
-        app.get('/health-check', (req, res, next)=> {
-            res.send('I am OK');
-        })
-        return this;
-    }
-
-    run(){
-        const { app, config:{ PORT } } =this;
-        app.listen(9800, (err) => {
-            if(err){
-                console.log(err);
-            }
-            console.log(`App is running on port ${PORT}`);
-        })
-    }
-}
-
-export default server; */
 import * as express from "express";
+import * as bodyParser from "body-parser";
+import { notFoundHandler, errorHandler } from './libs/routes';
+
 class Server{
     app
     constructor(private config){
@@ -39,19 +11,46 @@ class Server{
         this.setupRouts()
         return this;
     }
-    setupRouts(){
+    public setupRouts(){
         const { app }=this;
-        app.get('/health-check',(req,res,next)=>{
+        app.use('/health-check',(req, res)=>{
+            console.log("inside Second middleware");
             res.send("I am OK");
         });
 
+        this.app.use(notFoundHandler);
+        this.app.use(errorHandler);
+        this.app.use((req, res, next) => {
+            next({
+                error: "Not Found",
+                code: 404
+                
+            })
+        })
+
+        this.app.use((err, req, res, next) => {
+            console.log(err);
+            res.json(
+                {
+                    "error ": err.error,
+                    status : err.code,
+                    message : err. message || "Error",
+                    timeStamp: new Date()
+                 
+                }
+            )
+        });
         return this;
+    }
+    public initBodyParser(){
+        this.app.use(bodyParser.json( {type : 'application/**json'}))
     }
     run(){
         const {app, config:{PORT}}=this;
         app.listen(PORT,(err)=>{
             if (err) {
                 console.log( err );
+                
             }
             console.log(`App is running on port ${PORT}`);
 
