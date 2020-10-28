f0930d9276e9ff7928e28a7a5cc633f91b58e472
Sumit kumar Upadhyay <72761197+Sumitsuccessivetech@users.noreply.github.com>
2020-10-28 12:25:56 +0530

    Delete index.js

--- dist/utills/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.validateUsers = exports.hasPermission = void 0;
-const permission_1 = require("./permission");
-exports.hasPermission = permission_1.default;
-const validation_1 = require("./validation");
-exports.validateUsers = validation_1.default;
-//# sourceMappingURL=index.js.map
