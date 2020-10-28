fb6a049d6b7d8f2d56c792c255519596e93706cf
Sumit kumar Upadhyay <72761197+Sumitsuccessivetech@users.noreply.github.com>
2020-10-28 12:25:06 +0530

    Delete helpers.js

--- dist/utills/helpers.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function validateEmail(email) {
-    const pattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return (pattern.test(email));
-}
-exports.default = validateEmail;
-//# sourceMappingURL=helpers.js.map
