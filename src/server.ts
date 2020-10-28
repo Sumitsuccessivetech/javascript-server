409d45f15e7b8b299a9b36b878e9b82436fe7509
Sumit kumar Upadhyay <72761197+Sumitsuccessivetech@users.noreply.github.com>
2020-10-28 14:49:19 +0530

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,34 +1,3 @@
-/*import * as express from "express";
-class server{
-    app
-    constructor(private config){
-        this.app=express()
-    }
-    bootstrap(){
-        this.setupRoutes()
-        return this;
-    }
-
-    setupRoutes(){
-        const {app}= this;
-        app.get('/health-check', (req, res, next)=> {
-            res.send('I am OK');
-        })
-        return this;
-    }
-
-    run(){
-        const { app, config:{ PORT } } =this;
-        app.listen(9800, (err) => {
-            if(err){
-                console.log(err);
-            }
-            console.log(`App is running on port ${PORT}`);
-        })
-    }
-}
-
-export default server; */
 import * as express from "express";
 class Server{
     app
