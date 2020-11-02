aadf0dbd89daf8895005f0caf7432dda23bf1bad
Sumit Upadhyay <sumit.upadhyay@tr03.successivetech.com>
2020-11-02 12:36:28 +0530

    feat: chnages done

---
+++ extraTs/utills/interface.ts
@@ -0,0 +1,16 @@
+interface IUsers {
+    traineeEmail : string;
+    reviewerEmail : string;
+};
+
+type access ={
+    all : string[];
+    read : string[];
+    write : string[];
+    delete : string[];
+}
+
+interface IPermissions {
+    getUsers : access;
+    
+};
