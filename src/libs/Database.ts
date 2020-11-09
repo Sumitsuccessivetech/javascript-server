ff375da76764e305d1365ebb9bfa315cef3f27e6
Sumit Upadhyay <sumit.upadhyay@tr03.successivetech.com>
2020-11-09 15:20:12 +0530

    Feat: Models SetUp with Initial Seed

--- src/libs/Database.ts
+++ src/libs/Database.ts
@@ -1,23 +1,29 @@
 import * as mongoose from 'mongoose';
-
+import seedData from './seedData'
 
 class Database {
-    static open (mongoURL) {
-        return new Promise((resolve,reject)=> {
+    static open(mongoURL) {
+        return new Promise((resolve, reject) => {
             console.log('Inside open method');
-            mongoose.connect(mongoURL, {useNewUrlParser:true,useUnifiedTopology:true}, (err) => {
-                
-                if(err) {
+            mongoose.connect(mongoURL, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+
+                if (err) {
                     console.log(err);
                     reject(err);
                     return;
                 }
+                seedData();
                 resolve(undefined);
             });
         });
     }
-    static disconnect(){
-        console.log('Inside disconnect');
+    static disconnect() {
+        mongoose.disconnect((err) => {
+            if (err) {
+                console.log(err);
+            }
+            console.log('MongoDB connection close');
+        });
     }
 }
 export default Database;
