c24dbba590bddceccda22503c859becff534efbe
Sumit Upadhyay <sumit.upadhyay@tr03.successivetech.com>
2020-11-17 11:38:34 +0530

    Feat: Changes Done

--- src/libs/routes/validationHandler.ts
+++ src/libs/routes/validationHandler.ts
@@ -12,36 +12,39 @@ export default (config) => (req: Request, res: Response, next: NextFunction) => 
             console.log('val is', val);
             return req[val][key];
         });
-        const validatedValues = values.filter((val) => (val));
-        console.log('value is', validatedValues);
+        values.forEach(element => {
+           console.log(element); 
+        // const element = values.filter((val) => (val));
+        console.log('element is', element);
         if (obj.required) {
-            if (isNull(validatedValues)) {
+            if (isNull(element)) {
                 throw ({ status: 400, msg: `${key} is required field`, error: "Bad Request" })
             }
         }
         if (obj.string) {
-            if (!(typeof (validatedValues) === 'string')) {
+            if (!(typeof (element) === 'string')) {
                 throw ({ status: 400, msg: `${key}  should be a string`, error: "Bad Request" })
             }
         }
         if (obj.isObject) {
-            if (!(typeof (validatedValues) == 'object')) {
+            if (!(typeof (element) == 'object')) {
                 throw ({ status: 400, msg: `${key}  should be an Object`, error: "Bad Request" })
             }
         }
         if (obj.regex) {
             const regex = obj.regex;
-            if (!regex.test(validatedValues)) {
+            if (!regex.test(element)) {
                 throw ({ status: 400, msg: `${key} is not a valid Expression`, error: "Bad Request" })
             }
         }
 
         if (obj.number) {
-            if (isNaN(validatedValues || validatedValues=== undefined)){
+            if (isNaN(element || element=== undefined)){
                 throw ({ status: 400, msg: `${key}  must be a number`, error: "Bad Request" })
             }
         }
     })
+});
         next();
 };
 
