8cab69ac6024b150e430ed2b7b9d57fd7fd3fd8c
Sumit kumar Upadhyay <72761197+Sumitsuccessivetech@users.noreply.github.com>
2020-11-17 17:58:51 +0530

    Update notFoundRoutes.ts

--- src/libs/routes/notFoundRoutes.ts
+++ src/libs/routes/notFoundRoutes.ts
@@ -3,7 +3,6 @@ import {Request, Response, NextFunction} from 'express'
 export default (req:Request , res:Response, next: NextFunction) => {
     next({
         error: "Not Found",
-        code: 400,
-        msg: " "
+        code: 404
     })
 }
