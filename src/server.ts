be29f25ff6acfc80ee7979cfc6a64c54ea66cd20
Sumit Upadhyay <sumit.upadhyay@tr03.successivetech.com>
2020-11-19 17:34:00 +0530

    Feat: Changes Done!

--- src/server.ts
+++ src/server.ts
@@ -44,6 +44,7 @@ class Server {
                     }
                     else {
                         console.log(`App is running on port ${port}`);
+                        Database.disconnect();
                     }
                 });
             })
